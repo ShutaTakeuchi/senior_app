a39705c861090686ab0aceda280e875c320b9615
ShutaTakeuchi <shuta.takeuchi.080410@gmail.com>
2021-03-23 13:08:07 +0900

    fixed

--- src/senior_app/resources/views/home.blade.php
+++ src/senior_app/resources/views/home.blade.php
@@ -53,7 +53,7 @@
                             <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                 <div class="portfolio-item-caption-content text-center text-white"></div>
                             </div>
-                            <img class="img-fluid" src="images/portfolio/cabin.png" alt="" />
+                            <img class="img-fluid" src="images/portfolio/cart.png" alt="" />
                         </div>
                         </a>
                     </div>
