04ac7bfbdbea05573030b83a9c57fad2a2a315e9
ShutaTakeuchi <shuta.takeuchi.080410@gmail.com>
2021-04-09 14:27:14 +0900

    fixed

--- src/senior_app/resources/views/admin/home.blade.php
+++ src/senior_app/resources/views/admin/home.blade.php
@@ -25,12 +25,29 @@
                         ごはん
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">状況</h5>
+                        {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>注文依頼</th>
+                                @if($delivery_not_buy > 0)
+                                <td class="text-danger">{{ $delivery_not_buy }}</td>
+                                @else
+                                <td>{{ $delivery_not_buy }}</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <th>配達中</th>
+                                @if($delivery_delivering > 0)
+                                <td class="text-danger">{{ $delivery_delivering }}</td>
+                                @else
+                                <td>{{ $delivery_delivering }}</td>
+                                @endif
+                            </tr>
+                        </table>
                     </div>
                     <div class="card-footer text-muted">
-                        2 days ago
+                        <a href="{{ route('admin.search.delivery') }}" class="btn btn-primary">詳細</a>
                     </div>
                 </div>
             </div>
@@ -42,12 +59,45 @@
                         おかいもの
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">状況</h5>
+                        {{-- <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> --}}
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>注文依頼</th>
+                                @if ($item_not_buy > 0)
+                                <td class="text-danger">{{ $item_not_buy }}</td>
+                                @else
+                                <td>{{ $item_not_buy }}</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <th>注文済み</th>
+                                @if($item_bought > 0)
+                                <td class="text-danger">{{ $item_bought }}</td>
+                                @else
+                                <td>{{ $item_bought }}</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <th>入荷済み</th>
+                                @if($item_got > 0)
+                                <td class="text-danger">{{ $item_got }}</td>
+                                @else
+                                <td>{{ $item_got }}</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <th>配達中</th>
+                                @if($item_delivering > 0)
+                                <td class="text-danger">{{ $item_delivering }}</td>
+                                @else
+                                <td>{{ $item_delivering }}</td>
+                                @endif
+                            </tr>
+                        </table>
                     </div>
                     <div class="card-footer text-muted">
-                        2 days ago
+                        <a href="{{ route('admin.search.item') }}" class="btn btn-primary">詳細</a>
                     </div>
                 </div>
             </div>
