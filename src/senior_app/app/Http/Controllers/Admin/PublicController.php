7d3fccccc520dca7fbcf74d887acdd1a799d0b6b
ShutaTakeuchi <shuta.takeuchi.080410@gmail.com>
2021-04-22 16:03:01 +0900

    fixed

--- src/senior_app/app/Http/Controllers/Admin/PublicController.php
+++ src/senior_app/app/Http/Controllers/Admin/PublicController.php
@@ -52,7 +52,8 @@ class PublicController extends Controller
     */
     public function google_sheets_api($order, $category)
     {
-        $credentials_path = storage_path('app/json/credentials.json');
+        // $credentials_path = storage_path('app/json/credentials.json');
+        $credentials_path = storage_path('credentials.json');
         $client = new \Google_Client();
         $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
         $client->setAuthConfig($credentials_path);
